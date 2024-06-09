49c54edf23286579933decfb780e88a4baa5fb7d
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-06-09 22:09:15 +0400

    added project details page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,8 @@ import { ThirdwebProvider } from "thirdweb/react";
 import CroudfundingProvider from "./context/CroudfundingProvider";
 import "./index.css";
 
+// for starting run npx thirdweb@latest create --app, it generates a thirdweb configured app
+
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <ThirdwebProvider>
